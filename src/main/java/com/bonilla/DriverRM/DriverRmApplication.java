4cbd11b0413741aa221f5fc7960bedf51d43b704
Hector Bonilla <31080342+HectorBonilla17@users.noreply.github.com>
2022-05-12 22:48:25 -0500

    Add files via upload

---
+++ src/main/java/com/bonilla/DriverRM/DriverRmApplication.java
@@ -0,0 +1,13 @@
+package com.bonilla.DriverRM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverRmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriverRmApplication.class, args);
+	}
+
+}
